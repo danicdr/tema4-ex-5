6e92d25b65d7009ca36526fa6a0470d2a443375d
Daniel Codreanu <danielcdr2006@gmail.com>
2023-05-24 21:29:16 +0300

    Add project files.

---
+++ tema4 ex 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul");
+            int n = int.Parse(Console.ReadLine());
+            int sum = SumaNumerelor(n);
+            Console.WriteLine($"Suma numerelor de la 1 la {n} este: {sum}");
+        }
+
+        static int SumaNumerelor(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n + SumaNumerelor(n - 1);
+        }
+    }
+}
+//Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n, apelati - o si afisati-i rezultatul.
